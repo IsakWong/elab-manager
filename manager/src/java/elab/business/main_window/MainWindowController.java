2ca108e6c9d18bd08e380b2792a7b3a46b3d32b3
isakwong <isakwong@outlook.com>
2018-11-23 22:21:55 +0800

    加入修改Member页面

--- manager/src/java/elab/business/main_window/MainWindowController.java
+++ manager/src/java/elab/business/main_window/MainWindowController.java
@@ -30,6 +30,24 @@ public class MainWindowController extends BaseViewController {
     private double x_stage;
     private double y_stage;
 
+    EventHandler<MouseEvent> dragHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            Stage stage = (Stage) topBar.getScene().getWindow();
+            stage.setX(x_stage + event.getScreenX() - x1);
+            stage.setY(y_stage + event.getScreenY() - y1);
+        }
+    };
+    EventHandler<MouseEvent> pressHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            Stage stage = (Stage) topBar.getScene().getWindow();
+            x1 = event.getScreenX();
+            y1 = event.getScreenY();
+            x_stage = stage.getX();
+            y_stage = stage.getY();
+        }
+    };
     public void initializeController() {
         try {
 
@@ -49,32 +67,18 @@ public class MainWindowController extends BaseViewController {
                 }
             });
 
-            topBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    Stage stage = (Stage) topBar.getScene().getWindow();
-                    stage.setX(x_stage + event.getScreenX() - x1);
-                    stage.setY(y_stage + event.getScreenY() - y1);
-                }
-            });
-
-            topBar.setOnMousePressed(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    Stage stage = (Stage) topBar.getScene().getWindow();
-                    x1 = event.getScreenX();
-                    y1 = event.getScreenY();
-                    x_stage = stage.getX();
-                    y_stage = stage.getY();
-                }
-            });
+            tabPane.setOnMouseDragged(dragHandler);
+            tabPane.setOnMousePressed(pressHandler);
+            topBar.setOnMouseDragged(dragHandler);
+            topBar.setOnMousePressed(pressHandler);
 
             Tab userTab = new Tab();
             userTab.setText("成员管理");
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/member_tab.fxml"));
             Parent root = loader.load();
             userTab.setContent(root);
-            MemberTabController controller = loader.getController();
+            BaseViewController controller = loader.getController();
+            controller.initializeController();
 
             //controller.TabModule = module;
             tabPane.getTabs().add(userTab);
@@ -84,18 +88,24 @@ public class MainWindowController extends BaseViewController {
             Parent root2 = FXMLLoader.load(getClass().getResource("/assist_teaching_tab.fxml"));
             assistTab.setContent(root2);
             tabPane.getTabs().add(assistTab);
+            controller = loader.getController();
+            controller.initializeController();
 
             Tab registerTab = new Tab();
             registerTab.setText("签到与值班");
             Parent root3 = FXMLLoader.load(getClass().getResource("/register_tab.fxml"));
             registerTab.setContent(root3);
             tabPane.getTabs().add(registerTab);
+            controller = loader.getController();
+            controller.initializeController();
 
             Tab systemControlTab = new Tab();
             systemControlTab.setText("系统控制");
             Parent root4 = FXMLLoader.load(getClass().getResource("/system_ctrl_tab.fxml"));
             systemControlTab.setContent(root4);
             tabPane.getTabs().add(systemControlTab);
+            controller = loader.getController();
+            controller.initializeController();
 
 
 
--- manager/src/java/elab/business/module_tab/MemberTabController.java
+++ manager/src/java/elab/business/module_tab/MemberTabController.java
@@ -3,9 +3,15 @@ package elab.business.module_tab;
 import com.jfoenix.controls.JFXButton;
 import elab.application.BaseViewController;
 import elab.serialization.module.Module;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 public class MemberTabController extends BaseViewController {
 
@@ -28,9 +34,44 @@ public class MemberTabController extends BaseViewController {
     @FXML
     private ScrollPane contentPage;
 
+    Parent addMemberPage;
+    Parent modifyMemberPage;
 
+    EventHandler<MouseEvent> addMemberHandler =   new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/add_member_page.fxml"));
+                addMemberPage = loader.load();
+                contentPage.setContent(addMemberPage);
+            }
+            catch (IOException exp)
+            {
+
+            }
+
+        }
+    };
+
+
+    EventHandler<MouseEvent> modifyHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/modify_member_page.fxml"));
+                addMemberPage = loader.load();
+                contentPage.setContent(addMemberPage);
+            }
+            catch (IOException exp)
+            {
+
+            }
+        }
+    };
     @Override
     public void initializeController() {
+        addUser.setOnMouseClicked(addMemberHandler);
+        modifyInformation.setOnMouseClicked(modifyHandler);
 
     }
 }
